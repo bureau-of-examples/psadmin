7400382de12083a34f41964959bf959da8c93951
zhy2002 <hz141@uowmail.edu.au>
2015-12-21 21:43:42 +1100

    Added authors page.

---
+++ src/api/authorData.js
@@ -0,0 +1,20 @@
+
+var authors = [
+    {
+        id: 'cory-house',
+        firstName: 'Cory',
+        lastName: 'House'
+    },
+    {
+        id: 'scott-allen',
+        firstName: 'Scott',
+        lastName: 'Allen'
+    },
+    {
+        id: 'dan-wahlin',
+        firstName: 'Dan',
+        lastName: 'Wahlin'
+    }
+];
+
+exports.authors = authors;
--- src/components/homePage.js
+++ src/components/homePage.js
@@ -6,8 +6,11 @@ var Home = React.createClass({
     render: function () {
         return (
             <div className="jumbotron">
-                <h1>Pluralsight Administration</h1>
-                <p>React, React Router and Flux for ultra-responsive web apps.</p>
+                <div className="header">
+                    <h1>Pluralsight Administration</h1>
+
+                    <p>React, React Router and Flux for ultra-responsive web apps.</p>
+                </div>
             </div>
         );
     }
